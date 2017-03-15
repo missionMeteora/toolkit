8c1191fc0f2c6659d4ef8af69294a387050324ee
itsMontoya <josh@montoya.io>
2017-03-15 11:54:24 -0700

    Adding errorlist testing

---
+++ errors/errorList_test.go
@@ -0,0 +1,23 @@
+package errors
+
+import (
+	"testing"
+)
+
+func TestErrorList(t *testing.T) {
+	var errs ErrorList
+	errs.Push(Error("hello world"))
+	if errs.Err() == nil {
+		t.Fatal("error is nil when it should not be")
+	}
+
+	if errs.Len() != 1 {
+		t.Fatal("invalid errorlist length")
+	}
+
+	errs.ForEach(func(err error) {
+		if err.Error() != "hello world" {
+			t.Fatal("invalid error")
+		}
+	})
+}
